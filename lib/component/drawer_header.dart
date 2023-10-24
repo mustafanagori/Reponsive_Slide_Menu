c771ff24a662865055613429054bb7ed0110bc76
mustafanagori <mustafanagori89@gmail.com>
2023-10-24 10:18:59 +0500

    use basicUtlisPackage

--- lib/component/drawer_header.dart
+++ lib/component/drawer_header.dart
@@ -7,29 +7,42 @@ class CustomDrawerHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const DrawerHeader(
+    return DrawerHeader(
       decoration: BoxDecoration(color: Colors.black26),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          CircleAvatar(
-            radius: 40,
+          const CircleAvatar(
+            radius: 30,
             backgroundImage: NetworkImage(
               'https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg',
             ),
           ),
-          FittedBox(
+          const FittedBox(
             child: Text(
               "Muhammad",
               style: TextStyle(color: Colors.white),
             ),
           ),
-          FittedBox(
-            child: Text(
-              "Online",
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const FittedBox(
+                child: Text(
+                  "Online",
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.online_prediction,
+                  size: 22,
+                  color: Colors.green,
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,18 +1,56 @@
+import 'package:basic_utils/basic_utils.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({Key? key});
+
   @override
   Widget build(BuildContext context) {
-    //var w = MediaQuery.of(context).size.width;
-    return Container(
-      color: Colors.black45,
-      child: Center(
-        child: Text(
-          "Dashboard",
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
+    var w = MediaQuery.of(context).size.width;
+    var h = MediaQuery.of(context).size.height;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text(StringUtils.removeCharAtPosition('GhulamMustafa', 3)),
+        Text(StringUtils.pickOnly('98536254567', from: 3, to: 7)),
+        Text(StringUtils.truncate('This is a Dart Utility Library', 12)),
+        Text(StringUtils.removeExp('Hello This World', 'World')),
+        Text(StringUtils.generateRandomString(12)),
+        Text(MathUtils.getRandomNumber().toString()),
+      ],
     );
   }
 }
+        //Table(
+        //   children: [
+        //     TableRow(children: [
+        //       Container(
+        //         color: Colors.red,
+        //         width: 100,
+        //       ),
+        //       Container(
+        //         color: Colors.green,
+        //         width: 100,
+        //       ),
+        //       Container(
+        //         color: Colors.pink,
+        //         width: 100,
+        //       ),
+        //     ]),
+        //     TableRow(children: [
+        //       Container(
+        //         color: Colors.blue,
+        //         width: 100,
+        //       ),
+        //       Container(
+        //         color: Colors.black,
+        //         width: 100,
+        //       ),
+        //       Container(
+        //         color: Colors.grey,
+        //         width: 100,
+        //       ),
+        //     ]),
+        //   ],
+        // ),
